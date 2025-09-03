dd57a51c70295ef55ebff959705a501a8264990d
huangzhe <huangzhe@jbufa.com>
2025-09-03 14:16:43 +0800

    tiny

--- tools/yolo11_export.py
+++ tools/yolo11_export.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolo11n.pt")  # load an official model
+model = YOLO("/home/manu/tmp/yolo11n.pt")  # load an official model
 # model = YOLO("path/to/best.pt")  # load a custom trained model
 
 # Export the model
-model.export(format="onnx")
+model.export(format="onnx", imgsz=(384, 640), dynamic=True)
