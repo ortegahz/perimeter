cb5994bcd0fdda08e3e6c3b32a217d1b18e255e4
huangzhe <huangzhe@jbufa.com>
2025-10-14 16:04:47 +0800

    tiny

--- cpp/format_demo/update.sh
+++ cpp/format_demo/update.sh
@@ -2,6 +2,7 @@
 
 cp feature_processor.* /home/manu/nfs/VSCodeProject/smartboxcore/app/algorithm/ -rvf
 cp cores/face/FaceAnalyzer.* /home/manu/nfs/VSCodeProject/smartboxcore/app/algorithm/cores/face/ -rvf
+cp cores/face/PoseEstimator.* /home/manu/nfs/VSCodeProject/smartboxcore/app/algorithm/cores/face/ -rvf
 cp cores/personReid/PersonReid_dla.* /home/manu/nfs/VSCodeProject/smartboxcore/app/algorithm/cores/personReid/ -rvf
 
 echo "Done !!!"
