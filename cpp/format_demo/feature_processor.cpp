7cca67a6f08ad2a017135e9921f4f38230f7b644
manu <zxthz@126.com>
2025-12-16 11:56:02 +0800

    rm is_long_patch

--- cpp/format_demo/feature_processor.cpp
+++ cpp/format_demo/feature_processor.cpp
@@ -1968,7 +1968,7 @@ ProcessOutput FeatureProcessor::process_packet(const ProcessInput &input) {
                 cv::Mat patch;
                 gpu_patch.download(patch);
 
-                if (!is_long_patch(patch)) continue;
+                // if (!is_long_patch(patch)) continue;
 
                 // --- HANG FIX: Wrap Re-ID DLA call with a timeout ---
                 cv::cuda::GpuMat feat_mat_gpu;
